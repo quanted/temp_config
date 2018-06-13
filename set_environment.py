0be7d17b3b4e01a031e73f5ba631638ffbcb0138
Tom Purucker <purucker.tom@gmail.com>
2018-06-13 14:15:26 -0700

    added conditional for 007

--- set_environment.py
+++ set_environment.py
@@ -42,7 +42,8 @@ class DeployEnv(object):
             if self.docker_hostname == "ord-uber-vm003":
                 logging.info("Deploying on public server.. Setting IS_PUBLIC to True..")
                 _is_public = True  # only case to set True
-        elif self.docker_hostname == "ord-uber-vm005":
+        elif self.docker_hostname == "ord-uber-vm005" or self.docker_hostname == "ord-uber-vm007":
+            print("TEST: " + str(self.docker_hostname))
             logging.warning("Deploying to development server...")
             _env_file = 'docker_epa.env'
         else:
