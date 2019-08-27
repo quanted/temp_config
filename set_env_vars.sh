04840574210c5acebdb1f46cc2bc0acf4388ee7a
popenc <i.nickpope@gmail.com>
2019-08-27 09:29:05 -0400

    updated shell script to set env vars from .env files

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Sets env vars from .env file for unix-based systems
-# To run> . set_env_vars.sh env_file_of_choice
-
-ENV_FILE=$1  # reads argument for env filename
-
-export QED_CONFIG=$ENV_FILE  # used in docker-compose.yml for setting env vars in containers
-echo QED_CONFIG=$QED_CONFIG
-
+# Sets env vars from input file ("$1"):
 while read p; do
-	echo $p
-	export $p
-done <$ENV_FILE
+	key=${p%=*}
+    val=`echo ${p#*=} | sed -e 's/^"\|"$//g' -e 's/\\r//g'`
+    export $key=$val
+done <"$1"
